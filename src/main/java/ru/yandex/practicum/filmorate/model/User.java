990bab7b2ac2d0d652553ab0e2c4c433b3cdc8d1
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-08-28 19:04:27 +0300

    UPD: Add basic business-logic, add tests, add basic validation for model structures as film/user

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -33,7 +33,7 @@ public class User {
     }
 
     public String getName() {
-        if (name == null) {
+        if (name == null || name.equals("")) {
             setName(login);
         }
         return name;
