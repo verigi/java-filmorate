d3d28751f1f97287e71353b33741f63500603e1b
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-09-08 22:56:37 +0300

    UPD: try commit

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.utils.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.utils.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
