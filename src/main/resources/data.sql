cd499569f19d6c7801faf7529a4529f75af55173
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-09-28 20:42:24 +0300

    UPD: Add database, add dao, rebuild business-logic

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
-MERGE INTO GENRE (GENRE_ID, NAME)
-    VALUES (1, 'Comedy'),
-    (2, 'Drama'),
-    (3, 'Cartoon'),
-    (4, 'Thriller'),
-    (5, 'Action'),
-    (6, 'Documentary'),
-    (7, 'Biography');
+INSERT INTO GENRE (NAME)
+    VALUES ('Comedy'),
+    ('Drama'),
+    ('Cartoon'),
+    ('Thriller'),
+    ('Action'),
+    ('Documentary'),
+    ('Biography');
 
-MERGE INTO MPA (MPA_ID, NAME)
-    VALUES (1, 'G'),
-    (2, 'PG'),
-    (3, 'PG-13'),
-    (4, 'R'),
-    (5, 'NC-17');
+INSERT INTO MPA (NAME)
+    VALUES ('G'),
+    ('PG'),
+    ('PG-13'),
+    ('R'),
+    ('NC-17');
