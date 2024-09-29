c402f751c081181c8162c11c488854272fd00ee6
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-09-29 22:46:19 +0300

    UPD: Add database, add dao, rebuild business-logic

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.GenreNotFoundException;
 import ru.yandex.practicum.filmorate.model.film.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
@@ -33,8 +34,13 @@ public class GenreDbStorage implements GenreStorage {
     @Override
     public Optional<Genre> getGenre(Integer id) {
         String sql = "SELECT * FROM genre WHERE genre_id = ?";
-        return jdbcTemplate.query(sql, genreRowMapper, id)
+        Optional<Genre> genre = jdbcTemplate.query(sql, genreRowMapper, id)
                 .stream()
                 .findFirst();
+        if (genre.isEmpty()) {
+            log.warn("Genre with id " + id + " was not found.");
+            throw new GenreNotFoundException("Incorrect id");
+        }
+        return genre;
     }
 }
