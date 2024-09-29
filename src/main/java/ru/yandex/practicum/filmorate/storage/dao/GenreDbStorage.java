d1b5cf64e2a139967408df359e06e039e11a6a33
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-09-29 23:32:39 +0300

    UPD: Add database, add dao, rebuild business-logic

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.GenreNotFoundException;
-import ru.yandex.practicum.filmorate.exception.MpaNotFoundException;
 import ru.yandex.practicum.filmorate.model.film.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
