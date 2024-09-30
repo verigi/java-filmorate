1ef1aaf4ff3b19cda9513030f3ec74715fe691bd
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-09-30 16:42:51 +0300

    UPD: Add database, add dao, rebuild business-logic

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,41 +1,13 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.model.film.Film;
-import ru.yandex.practicum.filmorate.model.film.Mpa;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-class FilmDbStorageTests {
-
-	private final FilmStorage filmStorage;
-	private final JdbcTemplate jdbcTemplate;
+class FilmorateApplicationTests {
 
 	@Test
-	@DisplayName("Adding film to database")
-	void shouldAddFilmToDB() {
-		Film film = new Film();
-		film.setName("Test Film");
-		film.setDescription("Description");
-		film.setReleaseDate(LocalDate.of(2022, 1, 1));
-		film.setDuration(120);
-		film.setMpa(new Mpa(1, "G"));
-
-		Film addedFilm = filmStorage.addFilm(film);
-		assertNotNull(addedFilm.getId(), "Film ID should not be null after insertion");
-		assertEquals(film.getName(), addedFilm.getName(), "Film name should match");
+	void contextLoads() {
 	}
 
 }
