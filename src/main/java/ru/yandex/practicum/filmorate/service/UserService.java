1e1206166e93239214e818232c37b956edadd9d2
Ivan Zavorykin <zavorykin.dev@yandex.ru>
2024-10-01 18:32:49 +0300

    UPD: make additional logging

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -19,42 +19,52 @@ public class UserService {
     }
 
     public User addUser(User user) {
+        log.debug("Add user {}", user.getName());
         return storage.addUser(user);
     }
 
     public User updateUser(User user) {
+        log.debug("Updating user with id {}", user.getId());
         return storage.updateUser(user);
     }
 
     public User deleteUserById(Integer id) {
+        log.debug("Delete user with id {}", id);
         return storage.deleteUser(id);
     }
 
     public User getUserById(Integer id) {
+        log.debug("Getting user with id {} from database", id);
         return storage.getUser(id);
     }
 
     public Collection<User> getAllUsers() {
+        log.debug("Getting list of users");
         return storage.getAllUsers();
     }
 
     public void deleteAllUsers() {
+        log.debug("Delete all users");
         storage.deleteAllUsers();
     }
 
     public void addFriend(Integer userId, Integer friendId) {
+        log.debug("User with id {} add friend with id {}", userId, friendId);
         storage.addFriend(userId, friendId);
     }
 
     public void deleteFriend(Integer userId, Integer friendId) {
+        log.debug("User with id {} delete friend with id {}", userId, friendId);
         storage.deleteFriend(userId, friendId);
     }
 
     public Collection<User> getAllFriends(Integer id) {
+        log.debug("Get friend-list of user with id {}", id);
         return storage.getAllFriends(id);
     }
 
     public Collection<User> getCommonFriends(Integer firstUserId, Integer secondUserId) {
+        log.debug("Get common friend-list of user with id {} and user with id {}", firstUserId,secondUserId);
         return storage.getCommonFriends(firstUserId, secondUserId);
     }
 
